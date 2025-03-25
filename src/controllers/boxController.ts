f6fb4b5c0f2c93fcda9946ea9131559fa271393e
AzaelG26 <azaelgarcia096@gmail.com>
2025-03-25 14:51:28 -0600

    funcion para buscar cajas por id

--- src/controllers/boxController.ts
+++ src/controllers/boxController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { db } from "../database";
-import { vaults } from "../database/schemas";
+import {users, vaults} from "../database/schemas";
 
 const createbox = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -40,6 +40,26 @@ const createbox = async (req: Request, res: Response): Promise<void> => {
 };
 
     const showBoxByUserId = async (req: Request, res: Response): Promise<void> => {
-        
+        try {
+            const {id} = req.params;
+            if (!id) {
+                res.status(400).json({msg: "id is required"});
+                return;
+            }
+            const findBoxById = await db.query.vaults.findFirst({
+                where: (vaults, {eq}) => eq(vaults.id, id)
+            });
+            if (!findBoxById) {
+                res.status(404).json({msg: "Box not found"});
+                return;
+            }
+            res.status(200).json({
+                msg: "Box found",
+                box: findBoxById
+            });
+        }catch(err) {
+            res.status(500).json({ msg: "Server Error", err });
+            console.log(err);
+        }
     }
-export { createbox };
+export { createbox, showBoxByUserId };
--- src/routes/box.ts
+++ src/routes/box.ts
@@ -1,8 +1,9 @@
 import express, {Router} from "express";
-import {createbox} from "../controllers/boxController";
+import {createbox, showBoxByUserId} from "../controllers/boxController";
 import {verificarToken} from "../middlewares/verifyToken";
 const router: Router = express.Router();
 
 router.post("/createbox", verificarToken,createbox)
+router.get("/find-box/:id", showBoxByUserId)
 
 export default router;
